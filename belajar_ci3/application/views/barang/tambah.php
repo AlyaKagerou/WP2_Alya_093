be2e6438b43be55b416f295d691a16cf26e1564a
AlyaKagerou <alyazalfa46@gmail.com>
2023-04-04 16:42:17 +0700

    membuat data kasir

---
+++ belajar_ci3/application/views/barang/tambah.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php $this->load->view('partials/head.php') ?>
+</head>
+
+<body id="page-top">
+	<div id="wrapper">
+		<!-- load sidebar -->
+		<?php $this->load->view('partials/sidebar.php') ?>
+
+		
+			<!-- load footer -->
+			<?php $this->load->view('partials/footer.php') ?>
+		</div>
+	</div>
+	<?php $this->load->view('partials/js.php') ?>
+	<script src="<?= base_url('sb-admin/js/demo/datatables-demo.js') ?>"></script>
+	<script src="<?= base_url('sb-admin') ?>/vendor/datatables/jquery.dataTables.min.js"></script>
+	<script src="<?= base_url('sb-admin') ?>/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+</body>
+</html>
